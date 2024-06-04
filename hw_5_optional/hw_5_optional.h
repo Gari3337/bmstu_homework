decc96f65ca1fc16d7e736c72de2f2fdc4a3eead
Gari3337 <bujhm5555555@yandex.ru>
2024-06-04 18:40:25 +0300

    up

--- hw_5_optional/hw_5_optional.h
+++ hw_5_optional/hw_5_optional.h
@@ -62,12 +62,24 @@ class optional {
 //    if (!is_initialized_) {
 //      throw bad_optional_access();
 //    }
-//      return *(reinterpret_cast<T *>(&data_[0]));
+//      return reinterpret_cast<T &>(*(T *) (&data_[0]));
 //  }
+//
+//   return *(reinterpret_cast<T *>(&data_[0]));
+//
+//    const T &value() const & {
+//        if (!is_initialized_) throw bad_optional_access();
+//        return reinterpret_cast<const T &>(*(T *) &data_[0]);
+//    }
 
-    const T &value() const & {
-        if (!is_initialized_) throw bad_optional_access();
-        return reinterpret_cast<const T &>(*(T *) &data_[0]);
+    T &value() const &{  // NOLINT
+        if (!is_initialized_) {
+            throw bad_optional_access();
+        }
+        // char* ptr = data_;
+        // void* data_ptr = ptr;
+        // T* type_ptr = data_ptr;
+        return reinterpret_cast<T &>(*(T *) (&data_[0]));  // NOLINT
     }
 
 
