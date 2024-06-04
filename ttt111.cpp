f4c46166452b02c3396a9f3366fe481d6f047748
Gari3337 <bujhm5555555@yandex.ru>
2024-06-04 18:20:51 +0300

    up

--- ttt111.cpp
+++
@@ -1,6 +0,0 @@
-friend difference_type operator-(const list_iterator &end,
-                                 const list_iterator &begin) {
-    difference_type n =0 ;
-    for (; copy!=end; ++n, ++begin){}
-    return n;
-}
